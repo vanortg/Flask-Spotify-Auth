75addfe4feb4d5314eaa98cd5ba5c7e24c694914
Tom Casavant <tfcasavant@gmail.com>
2019-12-30 17:29:59 -0500

    Fixed errors associated with changes in apis

--- flask_spotify_auth.py
+++ flask_spotify_auth.py
@@ -2,12 +2,12 @@ import base64, json, requests
 
 SPOTIFY_URL_AUTH = 'https://accounts.spotify.com/authorize/?'
 SPOTIFY_URL_TOKEN = 'https://accounts.spotify.com/api/token/'
-RESPONSE_TYPE = 'code'   
+RESPONSE_TYPE = 'code'
 HEADER = 'application/x-www-form-urlencoded'
 REFRESH_TOKEN = ''
-    
+
 def getAuth(client_id, redirect_uri, scope):
-    data = "{}client_id={}&response_type=code&redirect_uri={}&scope={}".format(SPOTIFY_URL_AUTH, client_id, redirect_uri, scope) 
+    data = "{}client_id={}&response_type=code&redirect_uri={}&scope={}".format(SPOTIFY_URL_AUTH, client_id, redirect_uri, scope)
     return data
 
 def getToken(code, client_id, client_secret, redirect_uri):
@@ -18,14 +18,16 @@ def getToken(code, client_id, client_secret, redirect_uri):
         "client_id": client_id,
         "client_secret": client_secret
     }
-        
-     
-    encoded = base64.b64encode("{}:{}".format(client_id, client_secret))
-    headers = {"Content-Type" : HEADER, "Authorization" : "Basic {}".format(encoded)} 
+
+
+    auth_str = f"{client_id}:{client_secret}"
+    encoded = base64.urlsafe_b64encode(auth_str.encode()).decode()
+
+    headers = {"Content-Type" : HEADER, "Authorization" : "Basic {}".format(encoded)}
 
     post = requests.post(SPOTIFY_URL_TOKEN, params=body, headers=headers)
     return handleToken(json.loads(post.text))
-    
+
 def handleToken(response):
     auth_head = {"Authorization": "Bearer {}".format(response["access_token"])}
     REFRESH_TOKEN = response["refresh_token"]
@@ -39,5 +41,5 @@ def refreshAuth():
 
     post_refresh = requests.post(SPOTIFY_URL_TOKEN, data=body, headers=HEADER)
     p_back = json.dumps(post_refresh.text)
-    
+
     return handleToken(p_back)
--- startup.py
+++ startup.py
@@ -1,9 +1,11 @@
 from flask_spotify_auth import getAuth, refreshAuth, getToken
 
 #Add your client ID
+# YOU SHOULD USE os.environ['CLIENT']
 CLIENT_ID = "98c167e218bd483891f8638caaca19cb"
 
 #aDD YOUR CLIENT SECRET FROM SPOTIFY
+# YOU SHOULD USE os.environ['SECRET']
 CLIENT_SECRET = "e40d06af72524208bf0bbb048ba299a3"
 
 #Port and callback url can be changed or ledt to localhost:5000
@@ -12,7 +14,7 @@ CALLBACK_URL = "http://localhost"
 
 #Add needed scope from spotify user
 SCOPE = "streaming user-read-birthdate user-read-email user-read-private"
-#token_data will hold authentication header with access code, the allowed scopes, and the refresh countdown 
+#token_data will hold authentication header with access code, the allowed scopes, and the refresh countdown
 TOKEN_DATA = []
 
 
@@ -22,7 +24,7 @@ def getUser():
 def getUserToken(code):
     global TOKEN_DATA
     TOKEN_DATA = getToken(code, CLIENT_ID, CLIENT_SECRET, "{}:{}/callback/".format(CALLBACK_URL, PORT))
- 
+
 def refreshToken(time):
     time.sleep(time)
     TOKEN_DATA = refreshAuth()
